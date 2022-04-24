3fc013358185538a11c513ed73300776a883a673
StyxXx1337 <kefin87@gmail.com>
2022-04-24 12:54:17 +0900

    fix: mutex lock

--- 5_MultiThreading/5-Traffic_Lights/src/Intersection.cpp
+++ 5_MultiThreading/5-Traffic_Lights/src/Intersection.cpp
@@ -29,14 +29,11 @@ void WaitingVehicles::permitEntryToFirstInQueue()
 {
     std::lock_guard<std::mutex> lock(_mutex);
 
-    // get entries from the front of both queues
     auto firstPromise = _promises.begin();
     auto firstVehicle = _vehicles.begin();
 
-    // fulfill promise and send signal back that permission to enter has been granted
     firstPromise->set_value();
 
-    // remove front elements from both queues
     _vehicles.erase(firstVehicle);
     _promises.erase(firstPromise);
 }
@@ -69,33 +66,31 @@ std::vector<std::shared_ptr<Street>> Intersection::queryStreets(std::shared_ptr<
     return outgoings;
 }
 
-// adds a new vehicle to the queue and returns once the vehicle is allowed to enter
 void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
 {
     std::unique_lock<std::mutex> lck(_mtx);
     std::cout << "Intersection #" << _id << "::addVehicleToQueue: thread id = " << std::this_thread::get_id() << std::endl;
     lck.unlock();
 
-    // add new vehicle to the end of the waiting line
     std::promise<void> prmsVehicleAllowedToEnter;
     std::future<void> ftrVehicleAllowedToEnter = prmsVehicleAllowedToEnter.get_future();
     _waitingVehicles.pushBack(vehicle, std::move(prmsVehicleAllowedToEnter));
 
-    // wait until the vehicle is allowed to enter
     ftrVehicleAllowedToEnter.wait();
     lck.lock();
     std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
     
-    // FP.6b : use the methods TrafficLight::getCurrentPhase and TrafficLight::waitForGreen to block the execution until the traffic light turns green.
-    _trafficLight.waitForGreen();  
+    if (_trafficLight.getCurrentPhase()!=TrafficLightPhase::kgreen){
+      std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is waiting for green." << std::endl;
+      _trafficLight.waitForGreen();
+    }
+    std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " received green signal." << std::endl;
 
     lck.unlock();
 }
 
 void Intersection::vehicleHasLeft(std::shared_ptr<Vehicle> vehicle)
 {
-    //std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " has left." << std::endl;
-
     // unblock queue processing
     this->setIsBlocked(false);
 }
@@ -109,30 +104,22 @@ void Intersection::setIsBlocked(bool isBlocked)
 // virtual function which is executed in a thread
 void Intersection::simulate() // using threads + promises/futures + exceptions
 {
-    // FP.6a : In Intersection.h, add a private member _trafficLight of type TrafficLight. At this position, start the simulation of _trafficLight.
     _trafficLight.simulate();
-    // launch vehicle queue processing in a thread
     threads.emplace_back(std::thread(&Intersection::processVehicleQueue, this));
 }
 
 void Intersection::processVehicleQueue()
 {
-    // print id of the current thread
-    //std::cout << "Intersection #" << _id << "::processVehicleQueue: thread id = " << std::this_thread::get_id() << std::endl;
+    std::cout << "Intersection #" << _id << "::processVehicleQueue: thread id = " << std::this_thread::get_id() << std::endl;
 
-    // continuously process the vehicle queue
     while (true)
     {
-        // sleep at every iteration to reduce CPU usage
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
-        // only proceed when at least one vehicle is waiting in the queue
         if (_waitingVehicles.getSize() > 0 && !_isBlocked)
         {
             // set intersection to "blocked" to prevent other vehicles from entering
             this->setIsBlocked(true);
-
-            // permit entry to first vehicle in the queue (FIFO)
             _waitingVehicles.permitEntryToFirstInQueue();
         }
     }
@@ -140,13 +127,8 @@ void Intersection::processVehicleQueue()
 
 bool Intersection::trafficLightIsGreen()
 {
-   // please include this part once you have solved the final project tasks
-   
    if (_trafficLight.getCurrentPhase() == TrafficLightPhase::kgreen)
        return true;
    else
        return false;
-   
-
-  // return true; // makes traffic light permanently green
 } 
--- 5_MultiThreading/5-Traffic_Lights/src/TrafficLight.cpp
+++ 5_MultiThreading/5-Traffic_Lights/src/TrafficLight.cpp
@@ -6,36 +6,29 @@
 #include "TrafficLight.h"
 
 /* Implementation of class "MessageQueue" */
-
  
 template <typename T>
 T MessageQueue<T>::receive()
 {
-    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
-    // to wait for and receive new messages and pull them from the queue using move semantics. 
-    // The received object should then be returned by the receive function. 
-    std::unique_lock<std::mutex> lck;
+    std::unique_lock<std::mutex> lck (_mtx);
     _condition.wait(lck, [this]{ return !_queue.empty();});
 
     T message = std::move(_queue.back());
     _queue.pop_back();
-
+    std::cout << "Message Queue popped.\n";
     return message;
 }
 
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
 {
-    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
-    // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
     std::lock_guard<std::mutex> lck (_mtx);
-    _queue.push_back(msg); 
+    _queue.push_back(std::move(msg)); 
     _condition.notify_one();
 }
 
 
 /* Implementation of class "TrafficLight" */
-
 
 TrafficLight::TrafficLight()
 {
@@ -45,9 +38,6 @@ TrafficLight::TrafficLight()
 
 void TrafficLight::waitForGreen()
 {
-    // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
-    // runs and repeatedly calls the receive function on the message queue. 
-    // Once it receives TrafficLightPhase::kgreen, the method returns.
     while (true) {
       if (_messages.receive()==TrafficLightPhase::kgreen){
         return;
@@ -63,36 +53,31 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 
 void TrafficLight::simulate()
 {
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
     threads.push_back(std::move(std::thread(&TrafficLight::cycleThroughPhases, this)));  
 }
 
 // virtual function which is executed in a thread
 void TrafficLight::cycleThroughPhases()
 {
-    // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
-    // and toggles the current phase of the traffic light between red and green and sends an update method 
-    // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
-    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
-
     auto start = std::chrono::high_resolution_clock::now(); 
+    int max_cycle_duration = rand() % 3 + 4;
+
     while (true){
       std::this_thread::sleep_for(std::chrono::milliseconds(1));
       auto now = std::chrono::high_resolution_clock::now();
       auto duration = std::chrono::duration_cast<std::chrono::seconds>(now - start);
 
-      int cycle_duration = rand() % 2 + 4; 
      
-      if (duration.count() >= cycle_duration){ 
-        // toggle red/green
-        if (this->_currentPhase == TrafficLightPhase::kred) {
-          this->_currentPhase = TrafficLightPhase::kgreen;
+      if (duration.count() >= max_cycle_duration){ 
+        if (_currentPhase == TrafficLightPhase::kred) {
+          _currentPhase = TrafficLightPhase::kgreen;
         }
         else{
-          this->_currentPhase = TrafficLightPhase::kred;
+          _currentPhase = TrafficLightPhase::kred;
         }
-        // Send Update Method 
-        this->_messages.send(std::move(this->_currentPhase));     
+
+        start = now;
+        _messages.send(std::move(_currentPhase));     
       }
 
 
